e0c4f01cf31dac128205f8f517ea09e490294925
Martin Tracey <matracey@outlook.ie>
2021-01-05 23:32:53 -0800

    Updated gitignore

--- ValorantOverlay.App/Properties/Resources.Designer.cs
+++ ValorantOverlay.App/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ValorantStreamOverlay.Properties {
+namespace ValorantOverlay.App.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ValorantStreamOverlay.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ValorantStreamOverlay.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ValorantOverlay.App.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
