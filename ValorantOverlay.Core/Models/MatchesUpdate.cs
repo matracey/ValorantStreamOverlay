34f45d3583ac30f548280890249c62b1e2ba46a3
Martin Tracey <matracey@outlook.ie>
2021-01-09 17:36:38 -0800

    Added MatchManager service.

---
+++ ValorantOverlay.Core/Models/MatchesUpdate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ValorantOverlay.Core.Models
+{
+    public class MatchesUpdate
+    {
+        [JsonPropertyName("Version")]
+        public int Version { get; set; }
+
+        [JsonPropertyName("Subject")]
+        public string Subject { get; set; }
+
+        [JsonPropertyName("Matches")]
+        public List<Match> Matches { get; set; }
+    }
+}
---
+++ ValorantOverlay.Core/Services/MatchManager.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ValorantOverlay.Core.Models;
+
+namespace ValorantOverlay.Core.Services
+{
+    public interface IMatchManager
+    {
+        Task<MatchesUpdate> GetAllGameUpdatesAsync(Region region, string userId, string accessToken, string entitlementToken, int skip = 0, int pageSize = 20);
+    }
+
+    public class MatchManager : IMatchManager
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger<MatchManager> _logger;
+
+        public MatchManager(IHttpClientFactory clientFactory, ILogger<MatchManager> logger)
+        {
+            _clientFactory = clientFactory;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Fetches the latest RankedRating details from the Valorant API.
+        /// </summary>
+        /// <param name="region">The region to query for the given UserId.</param>
+        /// <param name="userId">The UserId to fetch the RankedRating details.</param>
+        /// <param name="accessToken">The AccessToken to use to authorize the API request.</param>
+        /// <param name="entitlementToken">The JWT entitlement token to include in the API request.01</param>
+        /// <returns>A tuple containing TierAfterUpdate and RankedRatingAfterUpdate</returns>
+        public async Task<MatchesUpdate> GetAllGameUpdatesAsync(Region region, string userId, string accessToken, string entitlementToken, int skip = 0, int pageSize = 20)
+        {
+            var client = _clientFactory.CreateClient(region);
+            var request = new HttpRequestMessage(HttpMethod.Get, $"mmr/v1/players/{userId}/competitiveupdates?startIndex={skip}&endIndex={pageSize}");
+            request.Headers.Add("Authorization", $"Bearer {accessToken}");
+            request.Headers.Add("X-Riot-Entitlements-JWT", entitlementToken);
+
+            try
+            {
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<MatchesUpdate>(content);
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to update the competitive rank.");
+                return null;
+            }
+        }
+    }
+}
