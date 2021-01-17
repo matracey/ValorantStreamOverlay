4727f9dd4a7345472314a169d4e3d4808cbc87c5
Martin Tracey <matracey@outlook.ie>
2021-01-17 01:39:01 -0800

    Added MainWindow logic.

--- ValorantOverlay.Wpf/App.xaml.cs
+++ ValorantOverlay.Wpf/App.xaml.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
+using ValorantOverlay.Core.Models;
+using ValorantOverlay.Core.Services;
+using ValorantOverlay.Wpf.Extensions;
 using ValorantOverlay.Wpf.Views;
 
 namespace ValorantOverlay.Wpf
@@ -15,16 +21,16 @@ namespace ValorantOverlay.Wpf
         private IConfigurationRoot _configuration;
         private ServiceProvider _serviceProvider;
 
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
             var builder = new ConfigurationBuilder()
              .SetBasePath(Directory.GetCurrentDirectory())
-             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
             _configuration = builder.Build();
 
             var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection);
+            await ConfigureServicesAsync(serviceCollection);
 
             _serviceProvider = serviceCollection.BuildServiceProvider();
 
@@ -32,9 +38,47 @@ namespace ValorantOverlay.Wpf
             mainWindow.Show();
         }
 
-        private void ConfigureServices(ServiceCollection services)
+        private async Task ConfigureServicesAsync(ServiceCollection services)
         {
-            services.AddTransient(typeof(MainWindow));
+            var endpoints = _configuration.GetSection("HttpEndpoints");
+
+            // System.Net
+            services.AddSingleton<CookieContainer>();
+
+            // System.Net.Http
+            services
+                .AddHttpClient(HttpClientName.NorthAmerica, c => c.BaseAddress = new Uri(endpoints[HttpClientName.NorthAmerica]))
+                .ConfigurePrimaryHttpMessageHandler(c => new HttpClientHandler { CookieContainer = c.GetRequiredService<CookieContainer>() });
+            services
+                .AddHttpClient(HttpClientName.Europe, c => c.BaseAddress = new Uri(endpoints[HttpClientName.Europe]))
+                .ConfigurePrimaryHttpMessageHandler(c => new HttpClientHandler { CookieContainer = c.GetRequiredService<CookieContainer>() });
+            services
+                .AddHttpClient(HttpClientName.Korea, c => c.BaseAddress = new Uri(endpoints[HttpClientName.Korea]))
+                .ConfigurePrimaryHttpMessageHandler(c => new HttpClientHandler { CookieContainer = c.GetRequiredService<CookieContainer>() });
+            services
+                .AddHttpClient(HttpClientName.AsiaPacific, c => c.BaseAddress = new Uri(endpoints[HttpClientName.AsiaPacific]))
+                .ConfigurePrimaryHttpMessageHandler(c => new HttpClientHandler { CookieContainer = c.GetRequiredService<CookieContainer>() });
+            services
+                .AddHttpClient(HttpClientName.AuthRiot, c => c.BaseAddress = new Uri(endpoints[HttpClientName.AuthRiot]))
+                .ConfigurePrimaryHttpMessageHandler(c => new HttpClientHandler { CookieContainer = c.GetRequiredService<CookieContainer>() });
+            services
+                .AddHttpClient(HttpClientName.EntitlementsAuthRiot, c => c.BaseAddress = new Uri(endpoints[HttpClientName.EntitlementsAuthRiot]))
+                .ConfigurePrimaryHttpMessageHandler(c => new HttpClientHandler { CookieContainer = c.GetRequiredService<CookieContainer>() });
+
+            // ValorantOverlay.Core.Models
+            services.AddSingleton<IRankList, RankList>(provider => _configuration.GetSection("Ranks").Get<RankList>());
+
+            // ValorantOverlay.Core.Services
+            services.AddSingleton<IRankManager, RankManager>();
+            services.AddSingleton<IRiotAuthenticator, RiotAuthenticator>();
+            services.AddSingleton<IMatchManager, MatchManager>();
+            services.AddSingleton<IStateManager, StateManager>();
+
+            // ValorantOverlay.Wpf.Extensions
+            await services.AddAppUserSettingsAsync();
+
+            services.AddTransient<MainWindow>();
+            services.AddTransient<SettingsWindow>();
         }
     }
 }
--- ValorantOverlay.Wpf/Views/MainWindow.xaml.cs
+++ ValorantOverlay.Wpf/Views/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ValorantOverlay.Core.Exceptions;
+using ValorantOverlay.Core.Models;
+using ValorantOverlay.Core.Services;
 
 namespace ValorantOverlay.Wpf.Views
 {
@@ -20,9 +16,103 @@ namespace ValorantOverlay.Wpf.Views
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly SettingsWindow _settingsWindow;
+        private readonly IStateManager _stateManager;
+
+        public MainWindow(SettingsWindow settingsWindow, IStateManager stateManager)
         {
             InitializeComponent();
+
+            _settingsWindow = settingsWindow;
+
+            _stateManager = stateManager;
+            _stateManager.RecentMatchesUpdating += StateManager_RecentMatchesUpdating;
+            _stateManager.RecentMatchesUpdated += StateManager_RecentMatchesUpdated;
+            _stateManager.RankUpdating += StateManager_RankUpdating;
+            _stateManager.RankUpdated += StateManager_RankUpdated;
+
+            Loaded += MainWindow_Loaded;
         }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await _stateManager.InitializeAsync();
+            }
+            catch (UserLoginNotSetException)
+            {
+                MessageBox.Show("Welcome! You must set your username and password in the settings menu.");
+                _settingsWindow.Show();
+            }
+            catch (UserLoginInvalidException)
+            {
+                MessageBox.Show("Login is invalid. Please fix login in settings.");
+                _settingsWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error has occurred. Technical details: {ex.Message}");
+            }
+        }
+
+        #region StateManager Events
+
+        private void StateManager_RecentMatchesUpdating()
+        {
+        }
+
+        private void StateManager_RecentMatchesUpdated(IEnumerable<Match> recentMatches)
+        {
+            var competitiveMatches = recentMatches.Where(m => m.TierAfterUpdate != 0).ToList();
+            var labels = new List<TextBlock> { recentGame1, recentGame2, recentGame3 };
+            for (int i = 0; i < labels.Count(); i++)
+            {
+                var match = competitiveMatches.ElementAtOrDefault(i);
+                var label = labels.ElementAtOrDefault(i);
+                var isLoss = match?.PointsEarned < 0;
+                var points = isLoss ? match.PointsEarned * -1 : match?.PointsEarned ?? 0;
+
+
+                if (points == 0)
+                {
+                    label.Foreground = Brushes.SlateGray;
+                    label.Text = $"{points:00}";
+                    continue;
+                }
+                label.Text = $"{(isLoss ? "-" : "+")}{points:00}";
+                label.Foreground = isLoss ? Brushes.Red : Brushes.LimeGreen;
+            }
+        }
+
+        private void StateManager_RankUpdating()
+        {
+        }
+
+        private void StateManager_RankUpdated((int rankNumber, int currentRp) obj)
+        {
+            rankingLabel.Text = _stateManager.CurrentRankName;
+
+            var bitmap = (BitmapImage)FindResource($"TX_CompetitiveTier_Large_{_stateManager.CurrentRank:00}");
+            rankIconBox.Source = bitmap;
+
+            rankPointsElo.Text = $"{_stateManager.CurrentRp} RR | {_stateManager.CurrentElo} TRR";
+        }
+
+        private void StateManager_ExceptionThrown(Exception ex)
+        {
+            if (ex is UserLoginNotSetException)
+            {
+                MessageBox.Show("Welcome! You must set your username and password in the settings menu.");
+                return;
+            }
+            if (ex is UserLoginInvalidException)
+            {
+                MessageBox.Show("Login is invalid. Please fix login in settings.");
+                return;
+            }
+            MessageBox.Show($"An error has occurred. Technical details: {ex.Message}");
+        }
+        #endregion
     }
 }
