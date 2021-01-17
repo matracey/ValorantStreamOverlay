cc6a45a628a03a6b5e20aa528677e685c24b7002
Martin Tracey <matracey@outlook.ie>
2021-01-16 18:16:31 -0800

    Added dependency injection to Wpf project.

--- ValorantOverlay.Wpf/App.xaml.cs
+++ ValorantOverlay.Wpf/App.xaml.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
+using ValorantOverlay.Wpf.Views;
 
 namespace ValorantOverlay.Wpf
 {
@@ -13,5 +12,29 @@ namespace ValorantOverlay.Wpf
     /// </summary>
     public partial class App : Application
     {
+        private IConfigurationRoot _configuration;
+        private ServiceProvider _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            _configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient(typeof(MainWindow));
+        }
     }
 }
