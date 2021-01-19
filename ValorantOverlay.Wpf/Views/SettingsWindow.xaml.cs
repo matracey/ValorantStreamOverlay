817de9aef543e024669e481fd6c14f635ee049ba
Martin Tracey <matracey@outlook.ie>
2021-01-18 21:14:20 -0800

    Added SettingsWindow view.

---
+++ ValorantOverlay.Wpf/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ValorantOverlay.Wpf.Views
+{
+
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
