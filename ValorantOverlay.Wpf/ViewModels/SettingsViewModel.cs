24f01e57a1bc393fc89186c98bc0def37552c60e
Martin Tracey <matracey@outlook.ie>
2021-01-18 21:22:22 -0800

    Added Region combobox control.

--- ValorantOverlay.Wpf/ViewModels/SettingsViewModel.cs
+++ ValorantOverlay.Wpf/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace ValorantOverlay.Wpf.ViewModels
+using System.Collections.Generic;
+using ValorantOverlay.Core.Models;
+
+namespace ValorantOverlay.Wpf.ViewModels
 {
     internal class SettingsViewModel : UserAccount
     {
+        public IList<Region> Regions => Region.All;
     }
 }
